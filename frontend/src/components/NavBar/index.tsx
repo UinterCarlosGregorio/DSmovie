35f77ef53f3b5fff8d76926c57520b34d98d6304
Uinter Carlos Gregorio <uintercgvga@hotmail.com>
2022-01-11 17:35:44 -0300

    git NavBar

---
+++ frontend/src/components/NavBar/index.tsx
@@ -0,0 +1,23 @@
+import { ReactComponent as GithubIcon } from "../../assets/img/github.svg";
+import "./styles.css";
+
+function NavBar() {
+    return (
+
+        <header>
+            <nav className="container">
+                <div className="DSmovie-nav-content">
+                    <h1>DSmovie</h1>
+                    <a href="https://github.com/UinterCarlosGregorio/DSmovie">
+                        <div className="DSmovie-contact-container">
+                            <GithubIcon />
+                            <p className="DSmovie-contact-link">/devsuperior</p>
+
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+export default NavBar;
